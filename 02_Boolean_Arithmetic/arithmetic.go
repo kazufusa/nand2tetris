03d8509ff9c1d281cc250dedb4ab956d31cd47e4
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-07-12 16:38:13 +0900

    feat(Boolean Arithmetic) add adders
    fix #2

---
+++ 02_Boolean_Arithmetic/arithmetic.go
@@ -0,0 +1,47 @@
+package arithmetic
+
+import (
+	logic "github.com/kazufusa/nand2tetris/01_Boolean_Logic"
+)
+
+type Bit = logic.Bit
+
+// halfAdder adds two bits and return bits.
+func halfAdder(a, b Bit) (sum, carry Bit) {
+	return logic.Xor(a, b), logic.Not(logic.Nand(a, b))
+}
+
+// fullAdder adds three bits and return bits.
+func fullAdder(a, b, c Bit) (sum, carry Bit) {
+	return logic.Xor(logic.Xor(a, b), c), logic.Or(
+		logic.Or(
+			logic.And(a, b), logic.And(b, c),
+		),
+		logic.And(c, a),
+	)
+}
+
+// Adder adds two n-bit numbers and return n-bit number. Adder ignores the
+// overflow bit.
+func Adder(a, b [16]Bit) [16]Bit {
+	ret := [16]Bit{}
+	var carry Bit
+	ret[0], carry = halfAdder(a[0], b[0])
+	ret[1], carry = fullAdder(a[1], b[1], carry)
+	ret[2], carry = fullAdder(a[2], b[2], carry)
+	ret[3], carry = fullAdder(a[3], b[3], carry)
+	ret[4], carry = fullAdder(a[4], b[4], carry)
+	ret[5], carry = fullAdder(a[5], b[5], carry)
+	ret[6], carry = fullAdder(a[6], b[6], carry)
+	ret[7], carry = fullAdder(a[7], b[7], carry)
+	ret[8], carry = fullAdder(a[8], b[8], carry)
+	ret[9], carry = fullAdder(a[9], b[9], carry)
+	ret[10], carry = fullAdder(a[10], b[10], carry)
+	ret[11], carry = fullAdder(a[11], b[11], carry)
+	ret[12], carry = fullAdder(a[12], b[12], carry)
+	ret[13], carry = fullAdder(a[13], b[13], carry)
+	ret[14], carry = fullAdder(a[14], b[14], carry)
+	ret[15], carry = fullAdder(a[15], b[15], carry)
+
+	return ret
+}
---
+++ 02_Boolean_Arithmetic/arithmetic_test.go
@@ -0,0 +1,97 @@
+package arithmetic
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHalfAdder(t *testing.T) {
+	var tests = []struct {
+		expected [2]Bit // sum and carry
+		given    [2]Bit // a, b, and c
+	}{
+		{[2]Bit{0, 0}, [2]Bit{0, 0}},
+		{[2]Bit{1, 0}, [2]Bit{0, 1}},
+		{[2]Bit{1, 0}, [2]Bit{1, 0}},
+		{[2]Bit{0, 1}, [2]Bit{1, 1}},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v", tt.given), func(t *testing.T) {
+			actual := [2]Bit{}
+			actual[0], actual[1] = halfAdder(tt.given[0], tt.given[1])
+			if actual != tt.expected {
+				t.Errorf("given(%v): expected %v, actual %v", tt.given, tt.expected, actual)
+			}
+		})
+	}
+}
+
+func TestFullAdder(t *testing.T) {
+	var tests = []struct {
+		expected [2]Bit // sum and carry
+		given    [3]Bit // a, b, and c
+	}{
+		{[2]Bit{0, 0}, [3]Bit{0, 0, 0}},
+		{[2]Bit{1, 0}, [3]Bit{0, 0, 1}},
+		{[2]Bit{1, 0}, [3]Bit{0, 1, 0}},
+		{[2]Bit{0, 1}, [3]Bit{0, 1, 1}},
+		{[2]Bit{1, 0}, [3]Bit{1, 0, 0}},
+		{[2]Bit{0, 1}, [3]Bit{1, 0, 1}},
+		{[2]Bit{0, 1}, [3]Bit{1, 1, 0}},
+		{[2]Bit{1, 1}, [3]Bit{1, 1, 1}},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v", tt.given), func(t *testing.T) {
+			actual := [2]Bit{}
+			actual[0], actual[1] = fullAdder(tt.given[0], tt.given[1], tt.given[2])
+			if actual != tt.expected {
+				t.Errorf("given(%v): expected %v, actual %v", tt.given, tt.expected, actual)
+			}
+		})
+	}
+}
+
+func TestAdder(t *testing.T) {
+	var tests = []struct {
+		expected [16]Bit
+		givenA   [16]Bit
+		givenB   [16]Bit
+	}{
+		{
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		{
+			[16]Bit{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			[16]Bit{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			[16]Bit{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		{
+			[16]Bit{1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			[16]Bit{1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			[16]Bit{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		{
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1},
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+		},
+		{
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+			[16]Bit{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v, %v", tt.givenA, tt.givenB), func(t *testing.T) {
+			actual := Adder(tt.givenA, tt.givenB)
+			if actual != tt.expected {
+				t.Errorf("given(%v,%v): expected %v, actual %v", tt.givenA, tt.givenB, tt.expected, actual)
+			}
+		})
+	}
+}
