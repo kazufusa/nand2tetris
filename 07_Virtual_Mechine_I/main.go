5e340af1f715c91c9364e75f228cde840ac8f479
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-09-07 16:38:57 +0900

    feat(VMI): add Parser

--- 07_Virtual_Mechine_I/main.go
+++ 07_Virtual_Mechine_I/main.go
@@ -1,5 +1,12 @@
 package vmI
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 // Specification
 // - Push/pop commands transfer data between the stack and memory segments
 // - Arithmetic-logical commands perform arithmetic and logical operations
@@ -34,10 +41,69 @@ const (
 	C_CALL
 )
 
-type Parser struct{}
+var (
+	reComment      = regexp.MustCompile(`//[^\n]*`)
+	reForwardSpace = regexp.MustCompile(`(?m)^\s*`)
+	reSpace        = regexp.MustCompile(`(?m)^\s+`)
+)
 
-func (p *Parser) hasMoreLines() bool { return false }
-func (p *Parser) advance()           {}
-func (p *Parser) commandType()       {}
-func (p *Parser) arg1() string       { return "" }
-func (p *Parser) arg2() int          { return 0 }
+type Parser struct {
+	lines []string
+	count int
+}
+
+func NewParser(s string) (*Parser, error) {
+	s = reComment.ReplaceAllString(s, "")
+	s = reForwardSpace.ReplaceAllString(s, "")
+	s = reSpace.ReplaceAllString(s, " ")
+	s = strings.ReplaceAll(s, "\r", "")
+	parser := Parser{}
+	parser.lines = strings.Split(s, "\n")
+	for i := len(parser.lines) - 1; i >= 0; i-- {
+		if parser.lines[i] == "" {
+			parser.lines = append(parser.lines[:i], parser.lines[i+1:]...)
+		}
+	}
+	return &parser, nil
+}
+
+func (p *Parser) hasMoreLines() bool {
+	return p.count < (len(p.lines) - 2)
+}
+
+func (p *Parser) advance() {
+	p.count++
+}
+
+func (p *Parser) commandType() (cmd Command, err error) {
+	args := strings.Split(p.lines[p.count], " ")
+	switch args[0] {
+	case "add", "sub", "neg", "eq", "gt", "lt", "and", "or", "no":
+		return C_ALITHMETIC, nil
+	case "push":
+		return C_PUSH, nil
+	case "pop":
+		return C_POP, nil
+	}
+
+	return C_POP, errors.New("invalid command")
+}
+
+func (p *Parser) arg1() (string, error) {
+	args := strings.Split(p.lines[p.count], " ")
+	if cmd, err := p.commandType(); err == nil && cmd == C_ALITHMETIC {
+		return args[0], nil
+	}
+	if len(args) < 2 {
+		return "", errors.New("arg1 is not exists")
+	}
+	return args[1], nil
+}
+
+func (p *Parser) arg2() (int, error) {
+	args := strings.Split(p.lines[p.count], " ")
+	if len(args) == 3 {
+		return strconv.Atoi(args[2])
+	}
+	return 0, errors.New("arg2 is not exists")
+}
--- 07_Virtual_Mechine_I/main_test.go
+++ 07_Virtual_Mechine_I/main_test.go
@@ -1,1 +1,15 @@
 package vmI
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	parser, _ := NewParser(`// Pushes and adds two constants
+  push constant 7
+  push constant 8
+  add
+  //`)
+	fmt.Printf("%#v\n", parser)
+}
